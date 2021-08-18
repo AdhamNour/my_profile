11a40926447b8237998aa8263dbe137ec0c2c707
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-08-18 06:08:01 +0200

    finsihed the skills section

--- src/components/Skills/CircularProgressSkill/CircularPorgressSkill.tsx
+++ src/components/Skills/CircularProgressSkill/CircularPorgressSkill.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Card } from "@material-ui/core";
-
+import sha512 from "crypto-js/sha512";
+import { useInViewport } from "react-in-viewport";
 import classes from "./CircularProgressSkill.module.css";
 
 export interface CircularProgressSkillProps {
@@ -14,17 +15,32 @@ const CircularProgressSkill: React.SFC<CircularProgressSkillProps> = ({
   percentage,
   imp,
 }) => {
+  const ref = useRef();
+  const { inViewport } = useInViewport(
+    ref,
+    {},
+    { disconnectOnLeave: false },
+    {}
+  );
+  const hashedItem = sha512(skillName);
+  let avatarColor = "#";
+  for (let index = 0; index < 6; index++) {
+    const element = hashedItem.words[index];
+    const newIndex = Math.abs(element % 16);
+    avatarColor = avatarColor.concat(newIndex.toString(16));
+  }
   let containerClassName = [classes.circularProgressSkillsElement];
   if (imp) {
     containerClassName.push(classes.circularProgressSkillsElementImportant);
   }
   return (
-    <Card className={containerClassName.join(" ")} raised>
+    <Card className={containerClassName.join(" ")} raised ref={ref}>
       <h2>{skillName}</h2>
       <div className={classes.singleChart}>
         <svg
           viewBox="0 0 36 36"
-          className={[classes.circularChart, classes.orange].join(" ")}
+          className={[classes.circularChart].join(" ")}
+          style={{ stroke: avatarColor }}
         >
           <path
             className={classes.circleBg}
@@ -33,14 +49,22 @@ const CircularProgressSkill: React.SFC<CircularProgressSkillProps> = ({
           a 15.9155 15.9155 0 0 1 0 -31.831"
           />
           <path
-            className={classes.circle}
+            className={[
+              classes.circle,
+              inViewport ? classes.animateProgress : "",
+            ].join(" ")}
             stroke-dasharray={`${percentage},100`}
             d="M18 2.0845
           a 15.9155 15.9155 0 0 1 0 31.831
           a 15.9155 15.9155 0 0 1 0 -31.831"
           />
-          <text x="18" y="20.35" className={classes.percentage}>
-            {percentage}
+          <text
+            x="18"
+            y="20.35"
+            className={classes.percentage}
+            style={{ fill: avatarColor }}
+          >
+            {percentage}%
           </text>
         </svg>
       </div>
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Skills.module.css";
+import aos from "aos";
+import "aos/dist/aos.css";
 import CircularProgressSkill from "./CircularProgressSkill/CircularPorgressSkill";
 export interface SkillsProps {}
 
 const Skills: React.SFC<SkillsProps> = () => {
+  useEffect(() => {
+    aos.init({ duration: 500 });
+  });
   const skills = [
     { skillName: "HTML5", precentage: 90, imp: "true" },
     { skillName: "CSS3", precentage: 90, imp: "true" },
@@ -11,14 +16,13 @@ const Skills: React.SFC<SkillsProps> = () => {
     { skillName: "Flutter", precentage: 95, imp: "true" },
     { skillName: "React", precentage: 95, imp: "true" },
     { skillName: "React Native", precentage: 95, imp: "true" },
-    { skillName: "MySQL/SQLite", precentage: 95,imp: "true" },
+    { skillName: "MySQL/SQLite", precentage: 95, imp: "true" },
     { skillName: "Python", precentage: 70 },
     { skillName: "C++", precentage: 75 },
     { skillName: "Native Android", precentage: 75 },
     { skillName: "Java", precentage: 80 },
     { skillName: "NodeJS", precentage: 80 },
     { skillName: "Unity", precentage: 65 },
-
   ];
   const SkillsUI = skills.map((skill, index) => (
     <CircularProgressSkill
@@ -29,9 +33,18 @@ const Skills: React.SFC<SkillsProps> = () => {
     />
   ));
   return (
-    <div className={classes.skillsContainer} id="Skills">
+    <div
+      className={classes.skillsContainer}
+      id="Skills"
+      data-aos="fade-zoom-in"
+    >
       <h1>My Skills</h1>
-      <div className={classes.circularProgressSkillsContainer}>{SkillsUI}</div>
+      <div
+        className={classes.circularProgressSkillsContainer}
+        data-aos="fade-up"
+      >
+        {SkillsUI}
+      </div>
     </div>
   );
 };
